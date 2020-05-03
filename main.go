3120daf05728c03e65a026518c0140d9fbd477df
s.kusuhara <s.kusuhara@trustridge.jp>
2020-05-03 23:56:07 +0900

    add route auth

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/err", err)
+
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signup_account)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/new", createThread)
+	mux.HandleFunc("/thread/new", postThread)
+	mux.HandleFunc("/thread/new", readThread)
+
+	server := &http.Server{
+		Addr: "0.0.0.0:8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
 }
